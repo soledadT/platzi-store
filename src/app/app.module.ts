767dfc8f72b2784d8ea38eaa70873ec4600b4ab4
soledadT <ntocci@gmail.com>
2021-04-06 10:19:27 -0300

    add modify to app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ProductComponent } from './product.component'
+import { ProductComponent } from './product.component';
+import { HomeComponent } from './home/home.component';
+import { ContactComponent } from './contact/contact.component';
+import { ProductsComponent } from './products/products.component'
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProductComponent
+    ProductComponent,
+    HomeComponent,
+    ContactComponent,
+    ProductsComponent
   ],
   imports: [
     BrowserModule,
